f121b831e25b9efc6e29b2cdae87bd28963f207e
Erdal Erden <57586690+ErdalErden@users.noreply.github.com>
2022-04-29 11:13:53 +0300

    donguler konusu işlendi

---
+++ ModernJavascriptES7/Javascript Temelleri/donguler.js
@@ -0,0 +1,39 @@
+// Loops - Döngüler
+
+//for loop--------------------
+//for(let i=0; i<10;i++){
+//    if(i==3){
+//        console.log(`en sevdiğim rakam `+i);
+//        continue;
+//    }
+//    if(i==6){
+//        console.log(`en sevmediğim rakam `+i);
+//        break;
+//    }
+//    console.log(i);
+//}
+
+//while loop--------------------
+
+//let i=0;
+//while(i<10) {
+//    console.log(i);
+//    i++;
+//}
+
+//do-while loop -----------------
+//let i=0;
+//do{
+//    console.log(i);
+//    i++;
+//} while(i<10);
+
+//------------------------------
+//let toplam=0
+//for(let i=10;i>0;i--){
+//    console.log(i);
+//    toplam+=i;
+//}
+//console.log(toplam);
+
+//----------------------------
